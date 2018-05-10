8ed7ec09ebbec9037f7b59e818b11b2a9fe806b7
Jakub Brehuv <kubek.breha@gmail.com>
2018-05-10 23:28:48 +0200

    add item c

--- adventure/adventure/item.c
+++ adventure/adventure/item.c
@@ -5,4 +5,24 @@
 //  Created by Jakub Brehuv on 10/05/2018.
 //
 
-#include "item.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"item.h"
+
+struct item* create_item(char* name, char* description, unsigned int properties){
+   if(name == NULL || description == NULL)
+       return NULL;
+
+    struct item* new_item = (struct item*)calloc(1, sizeof(struct item));
+    new_item->name = name;
+    new_item->description = description;
+    new_item->properties = properties;
+    
+    return new_item;
+}
+
+struct item* destroy_item(struct item* item){
+    free(item);
+    return NULL;
+}

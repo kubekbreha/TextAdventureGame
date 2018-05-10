9986871d15d2266d6324f23e9c6e9afba26a9acf
Jakub Brehuv <jakub.brehuv@ibm.com>
2018-05-10 16:53:35 +0200

    new xcode project

---
+++ adventure/adventure/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  adventure
+//
+//  Created by Jakub Brehuv on 10/05/2018.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

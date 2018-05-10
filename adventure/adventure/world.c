d1fc7c4024c24f5e8378d2d3bb2876b352e4edba
Jakub Brehuv <kubek.breha@gmail.com>
2018-05-10 23:29:11 +0200

    add world c

--- adventure/adventure/world.c
+++ adventure/adventure/world.c
@@ -5,4 +5,40 @@
 //  Created by Jakub Brehuv on 10/05/2018.
 //
 
-#include "world.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"world.h"
+
+struct container* create_world(){
+    struct container* cont = (struct container*)calloc(1, sizeof(struct container));
+  
+    cont->command = NULL;
+    cont->item = NULL;
+    cont->room = NULL;
+    cont->text = NULL;
+    cont->next = NULL;
+   
+    return cont;
+}
+
+struct container* add_room_to_world(struct container* world, struct room* room){
+    world->room = room;
+    return world;
+}
+
+struct container* destroy_world(struct container* world){
+    free(world);
+    return NULL;
+}
+
+struct room* get_room(struct container* world, char* name){
+    struct container* container = world;
+    
+    while (world != NULL){
+        if (container->room->name == name)
+            return container->room;
+        container = container->next;
+    }
+    return NULL;
+}

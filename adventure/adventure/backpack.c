80e88a1bf4ee27f8f23c8fd2d7217522066b5970
Jakub Brehuv <kubek.breha@gmail.com>
2018-05-12 18:49:25 +0200

    backpack dry fix

--- adventure/adventure/backpack.c
+++ adventure/adventure/backpack.c
@@ -31,7 +31,7 @@ struct backpack* destroy_backpack(struct backpack* backpack){
 
 bool add_item_to_backpack(struct backpack* backpack, struct item* item){
     if(item == NULL || backpack == NULL){
-        return NULL;
+        return false;
     }
     
     if(backpack->capacity >= backpack->size){
@@ -104,7 +104,7 @@ void delete_item_from_backpack(struct backpack* backpack, struct item* item){
 
 
 struct item* get_item_from_backpack(const struct backpack* backpack, char* name){
-    if(backpack == NULL){
+    if(backpack == NULL || name == NULL){
         return NULL;
     }
     if(strlen(name)<1 ){

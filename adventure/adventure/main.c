84c9de11a27c24f193f0c7251eb98f4fe8efbc43
Jakub Brehuv <kubek.breha@gmail.com>
2018-05-13 15:05:03 +0200

    world fixing

--- adventure/adventure/main.c
+++ adventure/adventure/main.c
@@ -7,6 +7,7 @@
 #include "backpack.h"
 #include "container.h"
 #include "game.h"
+#include "world.h"
 
 int main(int argc, const char * argv[]) {
     
@@ -16,23 +17,23 @@ int main(int argc, const char * argv[]) {
     
 //   printf( "--------------------TEST BACKPACK--------------------------------\n");
 
-    struct backpack* back = create_backpack(3);
-    struct item* itemA = create_item("SKALA1", "Pokial nie si sochar tak si ju prestan vsimat.", USABLE | EXAMINABLE);
-    struct item* itemB = create_item("SKALA2", "Pokial nie si sochar tak si ju prestan vsimat.", USABLE );
-    struct item* itemC = create_item("SKALA3", "Pokial nie si sochar tak si ju prestan vsimat.", USABLE );
+//    struct backpack* back = create_backpack(3);
+//    struct item* itemA = create_item("SKALA1", "Pokial nie si sochar tak si ju prestan vsimat.", USABLE | EXAMINABLE);
+//    struct item* itemB = create_item("SKALA2", "Pokial nie si sochar tak si ju prestan vsimat.", USABLE );
+//    struct item* itemC = create_item("SKALA3", "Pokial nie si sochar tak si ju prestan vsimat.", USABLE );
+//
+//    add_item_to_backpack(back, itemA);
+//    add_item_to_backpack(back, itemB);
+//    add_item_to_backpack(back, itemC);
+//
+//
+//    delete_item_from_backpack(back, itemB);
+//
+//    if(get_item_from_backpack(back, itemB->name) == NULL){
+//        printf("hure \n");
+//    }
 
-    add_item_to_backpack(back, itemA);
-    add_item_to_backpack(back, itemB);
-    add_item_to_backpack(back, itemC);
-
-
-    delete_item_from_backpack(back, itemB);
-
-    if(get_item_from_backpack(back, itemB->name) == NULL){
-        printf("hure \n");
-    }
-
-    print_backpack(back);
+  //  print_backpack(back);
 
 //    printf( "--------------------TEST ROOM--------------------------------\n");
 //    struct item* item = create_item("KLUC", "Touto vecou sa odomykaju ine veci.", MOVABLE | USABLE | EXAMINABLE);
@@ -80,6 +81,20 @@ int main(int argc, const char * argv[]) {
 //    };
 //
 //    print_room(room->items);
+    
+    
+    
+    struct room* item0 = create_room("intrak", "Velmi smradlava izba");
+    struct room* item1 = create_room("intrak2", "Velmi smradlava izba2");
+    struct room* item2 = create_room("intrak3", "Velmi smradlava izba3");
+    struct room* item3 = create_room("intrak4", "Velmi smradlava izba4");
+    
+    struct container* world =add_room_to_world(world, item0);
+    add_room_to_world(world, item1);
+    add_room_to_world(world, item2);
+    add_room_to_world(world, item3);
+    
+    printf("%s\n", get_room(world, "intrak2")->name);
     
     return 0;
 }
--- adventure/adventure/world.c
+++ adventure/adventure/world.c
@@ -7,8 +7,10 @@
 
 struct container* create_world(){
     struct container* world = (struct container*)calloc(1, sizeof(struct container));
-    world->next = NULL;
-    world->room = NULL;
+    world->room = create_room("intrakad", "Velmi smradlava izba");
+//    world->room->west = create_room("intrak2", "Velmi smradlava izba2");
+//    world->room->north = create_room("intrak3", "Velmi smradlava izba3");
+//    world->room->south = create_room("intrak4", "Velmi smradlava izba4");
     return world;
 }
 
@@ -22,16 +24,37 @@ struct container* add_room_to_world(struct container* world, struct room* room){
         return NULL;
     }
     
-    while(world->next != NULL){
-        world = world->next;
+    struct container* newWorld = (struct container*)malloc(sizeof(struct container*));
+
+    if(newWorld == NULL){
+        fprintf(stderr, "Unable to allocate memory for new node\n");
+        exit(-1);
     }
     
+    newWorld->room = room;
+    newWorld->next = NULL;
     
-    world->room = room;
-    world->next = NULL;
+    if(world->next == NULL){
+        world->next = newWorld;
+        printf("added at beginning\n");
+    }
     
+    else{
+        struct container* current = world;
+        while (true) {
+            if(current->next == NULL){
+                current->next = newWorld;
+                printf("added later\n");
+                break; // Change 3
+            }
+            current = current->next;
+        };
+    }
+    
+    free(newWorld);
     return world;
 }
+
 
 struct container* destroy_world(struct container* world){
     if(world == NULL){
@@ -53,8 +76,8 @@ struct room* get_room(struct container* world, char* name){
 
     struct container* container = world;
     
-    while (world->next != NULL){
-        if (container->room->name == name)
+    while (container != NULL){
+        if (strcmp(container->room->name,name))
             return container->room;
         container = container->next;
     }
